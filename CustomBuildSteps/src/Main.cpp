f6f68b7fc729c9f31a0cb868c23c35eda76e2a8e
LazyPanda07 <semengricenko@gmail.com>
2021-08-23 01:24:37 +0500

    Added comment

--- CustomBuildSteps/src/Main.cpp
+++ CustomBuildSteps/src/Main.cpp
@@ -1,3 +1,14 @@
+/*
+* Run through console with arguments
+* argv[0] - System reserved for path to executable
+* argv[1] - Path to binaries
+* argv[2] - Path to source files
+* argv[3] - Copy to path
+* argv[4] - Path to documentation
+* argv[5] - Name of folder that contains all data
+* argv[6...] - Paths to submodules
+*/
+
 #include <iostream>
 #include <filesystem>
 #include <unordered_map>
